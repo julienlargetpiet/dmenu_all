1403fcddad106ac210643bea4fd32e0fd254b792
iro0087 <114911243+iro0087@users.noreply.github.com>
2023-08-11 15:33:08 +0200

    Add files via upload

--- man_pdf.sh
+++ man_pdf.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+a=$(cat ~/all_media/all_media_read.sh | grep "pdf_viewer=")
+
+lnght=${#a}
+
+x=${a:11:$lnght}
+
+pdf_viewer=$x
+
 manual=$1
 
 man_out="${manual}.pdf"
 
-man -Tpdf $manual > $man_out && zathura $man_out
+man -Tpdf $manual > $man_out && $pdf_viewer $man_out
 
 rm $man_out
