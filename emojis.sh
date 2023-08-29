1b284a14c7e14a6d292561368b18236cb6426831
iro0087 <114911243+iro0087@users.noreply.github.com>
2023-08-29 22:41:12 +0200

    Add files via upload

---
+++ emojis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+a=$(cat ~/all_media/emojis.txt | dmenu -l 20 -p "emojis")
+
+if [ "$a" = "CANCEL" ]
+then
+
+        bash ~/all_media/all_media_read.sh 
+
+else
+
+        a=$(echo $a | cut -d "#" -f 2)
+
+        a=${a:1:1}
+
+        echo $a | xsel -b
+
+        exit 0
+
+        bash ~/all_media/all_media_read.sh 
+
+fi
